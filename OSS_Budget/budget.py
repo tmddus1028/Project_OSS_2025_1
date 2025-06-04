a2cf65beacce96a9cebd9b40b481b5249d860576
SeungYeon Lee <05dltmddus@naver.com>
2025-06-04 20:05:49 +0900

    [기능추가] 카테고리별 비중계산 기능추가

--- OSS_Budget/budget.py
+++ OSS_Budget/budget.py
@@ -16,12 +16,28 @@ class Budget:
             print("지출 내역이 없습니다.\n")
             return
         print("\n[지출 목록]")
-        for idx, e in enumerate(self.expenses, 1):
-            print(f"{idx}. {e}")
+        for idx, expense in enumerate(self.expenses, 1):
+            print(f"{idx}. {expense}")
         print()
 
     def total_spent(self):
-        total = sum(e.amount for e in self.expenses)
+        total = sum(expense.amount for expense in self.expenses)
         print(f"총 지출: {total}원\n")
 
+    def category_ratios(self):
+        if not self.expenses:
+            print("지출 내역이 없습니다.\n")
+            return
 
+        category_totals = {}
+        total = 0
+
+        for expense in self.expenses:
+            category_totals[expense.category] = category_totals.get(expense.category, 0) + expense.amount
+            total += expense.amount
+        print("\n[카테고리별 비중 ]")
+        for category, cat_total in category_totals.items():
+            ratio = (cat_total / total) * 100
+            print(f"{category}: {ratio:.1f}%")
+
+        print(f"\n총 지출: {total}원\n")
--- OSS_Budget/main.py
+++ OSS_Budget/main.py
@@ -1,6 +1,5 @@
 from budget import Budget
 
-
 def main():
     budget = Budget()
 
@@ -9,7 +8,8 @@ def main():
         print("1. 지출 추가")
         print("2. 지출 목록 보기")
         print("3. 총 지출 보기")
-        print("4. 종료")
+        print("4. 카테고리별 비중 보기")
+        print("5. 종료")
         choice = input("선택 > ")
 
         if choice == "1":
@@ -29,12 +29,14 @@ def main():
             budget.total_spent()
 
         elif choice == "4":
+            budget.category_ratios()
+
+        elif choice == "5":
             print("가계부를 종료합니다.")
             break
 
         else:
             print("잘못된 선택입니다.\n")
-
 
 if __name__ == "__main__":
     main()
